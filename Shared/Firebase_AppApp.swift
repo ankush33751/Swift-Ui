ce373ae4da9eccb489c49e5167729fdbc2ced64a
Ankush Anand <Ankushanand33@gmail.com>
2021-06-06 23:14:11 +0530

    Initial Commit

---
+++ Shared/Firebase_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_AppApp.swift
+//  Shared
+//
+//  Created by Ankush  Anand on 06/Jun/2021.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
